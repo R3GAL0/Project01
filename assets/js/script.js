2f726078502600eedb88176e0b14637d1e3e9176
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-08-24 10:05:37 -0400

    added second fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,30 +23,51 @@ var proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 function placeLocations(url){
     var proxyurl = "https://cors-anywhere.herokuapp.com/";
+    var locations = [];
     fetch(proxyurl + url)
         .then(function (response){
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(function (data) {
             console.log(data);
         });
 
-    var locations = [];
-    for (var i=0; i<data.length; i++){
-        locations.push(data.results[i].place_id);
-    }
-    return locations;
+    // for (var i=0; i<data.length; i++){
+    //     locations.push(data.results[i].place_id);
+    // }
+    // return locations;
 }
+placeLocations(url);
+
+// can only make basic requests of place details api. Else will be billed
+// Basic 'fields=': The Basic category includes the following fields: address_components, adr_address, business_status, formatted_address, geometry, icon, icon_mask_base_uri, icon_background_color, name, permanently_closed (deprecated), photo, place_id, plus_code, type, url, utc_offset, vicinity, wheelchair_accessible_entrance. 
+// need to include at least one basic 'fields=' request or it returns all options
+
+// place_id = ChIJD_SfHlk5tokRjbCVXAaBy3A
+var idTest = 'ChIJD_SfHlk5tokRjbCVXAaBy3A';
+function locationDetails (id) {
+    var proxyurl = "https://cors-anywhere.herokuapp.com/";
+    var locationURL = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' + id + '&fields=formatted_address&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc';
+    fetch(proxyurl + locationURL)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
+
+}
+
+locationDetails(idTest);
+// 'https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJD_SfHlk5tokRjbCVXAaBy3A&fields=formatted_address&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc
+
 
 
 // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mcdonalds&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc
 
-// retrieve specifications from index.html
-// input into query
-// fetch locations
-// fetch location details
-// print location details
 
 var foodTypeEl = document.querySelector('#filter-food-options');
 var userLocationEl = document.querySelector('#filter-user-location');
@@ -78,11 +99,14 @@ searchBtnEl.addEventListener('click', function (){
         searchQuery = searchQuery.concat('%20expensive');
     } // expensive
 
-    // alternative location
+    // alternative location ------------------------------------
     if ($(userLocationEl).children().eq(1).val() != ''){
         var locationTemp = '%20' + $(userLocationEl).children().eq(1).val().replace(' ', '%20');
         searchQuery = searchQuery.concat(locationTemp);
     }
     console.log('final: ' + searchQuery);
 
+    // perform a fetch with user query
+    // perform a fetch with location id
+    // print details to the page
 });
