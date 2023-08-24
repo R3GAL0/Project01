e9b5fe64bb7a87ab26a8c00633ca8a3ffa96b9bc
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-08-24 10:56:34 -0400

    can create lis on the page now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,38 @@
-
-// google web search api
-
-var apiKey = 'b8ddaabfe8msha35204713d89a7bp1c84d9jsn7aa75155a376'
-
-// google places api key
-// AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc
-
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=restaurant&name=harbour&key=YOUR_API_KEY
-
-// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?parameters
+var foodTypeEl = document.querySelector('#filter-food-options');
+var userLocationEl = document.querySelector('#filter-user-location');
+var pricesEl = document.querySelector('#filter-div-prices');
+var searchBtnEl = document.querySelector('#search-button');
+var favouritesEl = document.querySelector('#favourites-list');
+var searchResultsEl = document.querySelector('#search-results');
+// google maps places & details api key
+var apiKey = 'AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc'
 
 
-// can use place search to get an id, then place details for details on that id
-
+// use place search to get a place_id from a general query, then place details for details on that id
 var query = 'indian%20food';
-// var url =  'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + query + '&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc'
-var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20Sydney&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc';
 var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + query + '&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc';
-        //    https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants%20in%20Sydney&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc
 var proxyurl = "https://cors-anywhere.herokuapp.com/";
 // https://stackoverflow.com/questions/28359730/google-place-api-no-access-control-allow-origin-header-is-present-on-the-req
 
-function placeLocations(url){
+function placeLocations(url) {
     var proxyurl = "https://cors-anywhere.herokuapp.com/";
-    var locations = [];
+    // var locations = [];
     fetch(proxyurl + url)
-        .then(function (response){
+        .then(function (response) {
             // console.log(response);
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            for (var i=0; i<data.length; i++){
+                // locations.push(data.results[i].place_id);
+                locationDetails(data.results[i].place_id);
+            }
+            return locations;
         });
 
-    // for (var i=0; i<data.length; i++){
-    //     locations.push(data.results[i].place_id);
-    // }
-    // return locations;
 }
-placeLocations(url);
+// placeLocations(url); // for testing above
 
 // can only make basic requests of place details api. Else will be billed
 // Basic 'fields=': The Basic category includes the following fields: address_components, adr_address, business_status, formatted_address, geometry, icon, icon_mask_base_uri, icon_background_color, name, permanently_closed (deprecated), photo, place_id, plus_code, type, url, utc_offset, vicinity, wheelchair_accessible_entrance. 
@@ -46,61 +40,67 @@ placeLocations(url);
 
 // place_id = ChIJD_SfHlk5tokRjbCVXAaBy3A
 var idTest = 'ChIJD_SfHlk5tokRjbCVXAaBy3A';
-function locationDetails (id) {
+async function locationDetails(id) {
     var proxyurl = "https://cors-anywhere.herokuapp.com/";
-    var locationURL = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' + id + '&fields=formatted_address&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc';
+    var locationURL = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' + id + '&fields=formatted_address,business_status,icon,name,type,url,wheelchair_accessible_entrance&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc';
+    // does type contain resturant, is the buisness_status=true
     fetch(proxyurl + locationURL)
         .then(function (response) {
-            console.log(response);
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-        })
+            var resturant = [data.result.name, data.result.formatted_address, data.result.icon, data.result.wheelchair_accessible_entrance, data.result.business_status, data.result.url];
+            if (resturant[3]) {
+                resturant[3] = 'Yes';
+            } else {
+                resturant[3] = 'No';
+            }
+            console.log('resturant: ' + resturant);
+            // print details to the page
+            var li = document.createElement('li');
+            var btn = document.createElement('button');
+            li.textContent = 'Name: ' + resturant[0] + '\n Address: ' + resturant[1] + '\nGoogle Maps URL: ' + resturant[5] + '\n Wheelchair Accessable: ' + resturant[3] + '\n Business Status: ' + resturant[4];
+            searchResultsEl.append(li)
+            searchResultsEl.li.append(btn);
+            // make the url an anchor tag, add a favourite button
+        });
 
 
 }
 
-locationDetails(idTest);
-// 'https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJD_SfHlk5tokRjbCVXAaBy3A&fields=formatted_address&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc
+var test = locationDetails(idTest);
+console.log('outside the function' + test);
 
 
 
-// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mcdonalds&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyBWAZHdf5zRqq6liQdqOjUEEIqyxkdDzAc
-
-
-var foodTypeEl = document.querySelector('#filter-food-options');
-var userLocationEl = document.querySelector('#filter-user-location');
-var pricesEl = document.querySelector('#filter-div-prices');
-var searchBtnEl = document.querySelector('#search-button');
-
-searchBtnEl.addEventListener('click', function (){
+searchBtnEl.addEventListener('click', function () {
     var searchQuery = $('#search-bar').val();
     // if statements to check the checkboxes
     // if true add to query
     // ethnic food options --------------------------------
-    if ($(foodTypeEl).children().eq(1).children().eq(0).children().eq(0).prop('checked')){
+    if ($(foodTypeEl).children().eq(1).children().eq(0).children().eq(0).prop('checked')) {
         searchQuery = searchQuery.concat('%20italian');
     } // italian
-    if ($(foodTypeEl).children().eq(1).children().eq(1).children().eq(0).prop('checked')){
+    if ($(foodTypeEl).children().eq(1).children().eq(1).children().eq(0).prop('checked')) {
         searchQuery = searchQuery.concat('%20mexican');
     } // mexican
-    if ($(foodTypeEl).children().eq(1).children().eq(2).children().eq(0).prop('checked')){
+    if ($(foodTypeEl).children().eq(1).children().eq(2).children().eq(0).prop('checked')) {
         searchQuery = searchQuery.concat('%20chinese');
     } // chinese
     // price range -----------------------------------------
-    if ($(pricesEl).children().eq(1).children().eq(0).children().eq(0).prop('checked')){
+    if ($(pricesEl).children().eq(1).children().eq(0).children().eq(0).prop('checked')) {
         searchQuery = searchQuery.concat('%20cheap');
     } // cheap
-    if ($(pricesEl).children().eq(1).children().eq(1).children().eq(0).prop('checked')){
+    if ($(pricesEl).children().eq(1).children().eq(1).children().eq(0).prop('checked')) {
         searchQuery = searchQuery.concat('%20moderately%20priced');
     } // moderately priced
-    if ($(pricesEl).children().eq(1).children().eq(2).children().eq(0).prop('checked')){
+    if ($(pricesEl).children().eq(1).children().eq(2).children().eq(0).prop('checked')) {
         searchQuery = searchQuery.concat('%20expensive');
     } // expensive
 
     // alternative location ------------------------------------
-    if ($(userLocationEl).children().eq(1).val() != ''){
+    if ($(userLocationEl).children().eq(1).val() != '') {
         var locationTemp = '%20' + $(userLocationEl).children().eq(1).val().replace(' ', '%20');
         searchQuery = searchQuery.concat(locationTemp);
     }
@@ -110,3 +110,6 @@ searchBtnEl.addEventListener('click', function (){
     // perform a fetch with location id
     // print details to the page
 });
+
+
+// event listener for liked resturants
